3d2599558c2645546810e90f10ed28946c4d6922
K.E.E.R.T.H.A.N.A <KEERTHANA1.K@lntinfotech.com>
2020-12-16 11:02:31 +0530

    upated

---
+++ Sample application project/Sample application project/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_application_project
+{
+    partial class Program
+    {
+        void PrintWelcome()
+        {
+            Console.WriteLine("Welcome");
+        }
+    }
+}
